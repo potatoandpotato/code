ccd882687fc229fdb2af6607ad3e1bc479dbe20e
potatoandpotato <1572779471@qq.com>
2017-07-14 09:27:57 +0800

    cnode

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component{
+	render(){
+		return(
+			<div>
+				<h2>此网站为***版权，如有雷同纯属巧合！！！</h2>
+			</div>
+		)
+	}
+}  
+
+export default Footer
